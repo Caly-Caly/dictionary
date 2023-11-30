ac28e23da8ec49853aba50d7f895195b9b66005e
Caly-Caly <calystaaponte+github@gmail.com>
2023-11-29 22:29:51 -0500

    added search input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">Hello</header>
+        <main>
+          <Dictionary />
+        </main>
+        <small>
+          <footer className="App-footer">Coded by Calysta Aponte</footer>
+        </small>
+      </div>
     </div>
   );
 }
