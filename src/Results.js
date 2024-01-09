022825cf5aead61cd609c283bd8b7a273a3c1fb1
Caly-Caly <calystaaponte+github@gmail.com>
2024-01-08 20:39:58 -0500

    Added the phonetics to the searched word

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Meaning from "./Meaning";
 
+
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h1>{props.results.word}</h1>
+       <h2>( {props.results.phonetic} )</h2> 
+       <br />
         {props.results.meanings.map(function (meanings, index) {
           return (
             <div key={index}>
